363b117c6b7f8ef56c9adf8641a8508e62fa34b5
Scarlett Samantha Verheul <scarlett.verheul@gmail.com>
2025-01-01 07:59:32 +0100

    🌱 Added libaryItem to the seed file to generate 2-15 items per library

--- core/seeders/user.py
+++ core/seeders/user.py
@@ -2,12 +2,13 @@ from sqlalchemy.exc import IntegrityError
 from flask_sqlalchemy import SQLAlchemy
 from models.user import User
 from models.library import Library
+from models.library_item import LibraryItem
 from faker import Faker
 
 
 class UserSeeder:
     """
-    Seeder class to populate the database with User and Library records.
+    Seeder class to populate the database with User, Library, and LibraryItem records.
     """
 
     def __init__(self, db: SQLAlchemy):
@@ -16,7 +17,7 @@ class UserSeeder:
 
     def run(self):
         """
-        Seed the database with User and Library records.
+        Seed the database with User, Library, and LibraryItem records.
         """
         # Generate a password for all users
         password, salt = User._generate_password("default_password")
@@ -60,9 +61,9 @@ class UserSeeder:
             except IntegrityError:
                 self.db.session.rollback()
 
-    def _create_libraries_for_user(self, user):
+    def _create_libraries_for_user(self, user: User):
         """
-        Generate a few libraries for a given user.
+        Generate libraries and library items for a given user.
         """
         for _ in range(self.faker.random_int(min=1, max=5)):
             library = Library(
@@ -74,6 +75,33 @@ class UserSeeder:
 
             try:
                 self.db.session.add(library)
+                self.db.session.commit()
+                self._create_library_items_for_library(library)
+            except IntegrityError:
+                self.db.session.rollback()
+
+    def _create_library_items_for_library(self, library: Library):
+        """
+        Generate library items for a given library.
+        """
+        for _ in range(self.faker.random_int(min=2, max=15)):
+            library_item = LibraryItem(
+                library_id=library.id,
+                name=self.faker.word(),
+                description=self.faker.text(max_nb_chars=150),
+                mime_type=self.faker.mime_type(),
+                file_size=self.faker.random_int(
+                    min=1024, max=10485760
+                ),  # 1 KB to 10 MB
+                file_path=None
+                if self.faker.boolean(chance_of_getting_true=30)
+                else self.faker.file_path(),
+                is_public=self.faker.boolean(chance_of_getting_true=70),
+                owner_id=library.owner_id,
+            )
+
+            try:
+                self.db.session.add(library_item)
                 self.db.session.commit()
             except IntegrityError:
                 self.db.session.rollback()
